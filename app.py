376cab6237e3a87aa82c5f32b733cadf2ae5e15a
KernelPlusAcademy <KernelPlusAcademy@gmail.com>
2025-05-31 21:01:37 -0400

    app.py

--- app.py
+++ app.py
@@ -1,16 +1,13 @@
 from flask import Flask
 from flask_socketio import SocketIO
-from auth.auth_routes import auth_blueprint
-from terminal.shell_runner import terminal_blueprint
-import os
 
 app = Flask(__name__)
 app.secret_key = 'super_secret_key'
 socketio = SocketIO(app)
 
-# Register Blueprints
+# Import routes AFTER app is defined
+from auth.auth_routes import auth_blueprint
+from terminal.shell_runner import terminal_blueprint
+
 app.register_blueprint(auth_blueprint)
 app.register_blueprint(terminal_blueprint)
-
-if __name__ == "__main__":
-    socketio.run(app, host="0.0.0.0", port=5000)
